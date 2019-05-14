f0cb4ab168222a44fc10c4ae39a699e55edad14f
ivankukarin <ikukarin@yandex.ru>
2019-05-14 12:39:46 +0400

    Незакончено

--- js/app.js
+++ js/app.js
@@ -4,21 +4,32 @@ let arrayPrices = [200, 550, 4000, 23, 58, 5000, 485, 711];
 let minGamePoints = 10000;
 let strings = ["Ваш баланс"];
 
+
+const sum = () => 
+	{
+	let sum = 0;
+	for (let value of arrayPrices) 
+		{sum += value};
+	console.log(sum);
+	return sum;
+	};
+
+
+
+const bonus = () => {
+	if (sum() > minGamePoints) {
+	return (Math.round(sum() * 5 /100)) }
+	
+	console.log(bonus());
+	
+	};
+
+bonus();
+
 const formatGamePoints = (strings, array) =>
 	
 	{
-
 	const yourBalance = strings[0];
-
-	let sum = 0;
-	for (let value of array) 
-		{sum += value};
-	console.log(sum);
-
-	let bonus;
-	if (sum > minGamePoints) {
-	bonus = (Math.round(sum * 5 /100)) }
-	else console.log("У вас недостаточно бонусных баллов");
 	
 	let num2str;
     let n = Math.abs(bonus) % 100; 
@@ -28,21 +39,10 @@ const formatGamePoints = (strings, array) =>
     if (n1 > 1 && n1 < 5) { num2str = textForms[1]; };
     if (n1 == 1) { num2str = textForms[0]; }
 
-    
-    // console.log(yourBalance);
-    // console.log(bonus);
-    // console.log(n);
-    // console.log(n1);
-    // console.log(num2str);
-
     return `${yourBalance}${bonus} ${num2str}`;
 }
 
 const output = formatGamePoints`Ваш баланс: ${arrayPrices}`;
 console.log(output);
-
-
-
-let arrayPoints = [74989, 74990, 84990, 62000, 58480, 61800];
 
 
