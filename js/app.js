c827326b39fb6635e3e1bf1e7c8fd117772b9d86
ivankukarin <ikukarin@yandex.ru>
2019-05-14 23:09:37 +0400

    not good

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ let minGamePoints = 10000;
 let strings = ["Ваш баланс"];
 
 
-const sum = () => 
+const sum = (array) => 
 	{
 	let sum = 0;
-	for (let value of arrayPrices) 
+	for (let value of array) 
 		{sum += value};
 	console.log(sum);
 	return sum;
@@ -16,15 +16,14 @@ const sum = () =>
 
 
 
-const bonus = () => {
-	if (sum() > minGamePoints) {
-	return (Math.round(sum() * 5 /100)) }
-	
-	console.log(bonus());
-	
+const bonus = (array) => {
+	if (sum(array) > minGamePoints) {
+	return (Math.round(sum(array) * 5 /100)) }
+	else { return 0 };
 	};
 
-bonus();
+bonus(arrayPrices);
+console.log(bonus(arrayPrices));
 
 const formatGamePoints = (strings, array) =>
 	
@@ -32,16 +31,19 @@ const formatGamePoints = (strings, array) =>
 	const yourBalance = strings[0];
 	
 	let num2str;
-    let n = Math.abs(bonus) % 100; 
+    let n = Math.abs(bonus(array)) % 100; 
     let n1 = n % 10;
     let textForms = ["балл", "балла", "баллов"];
     if (n > 10 && n < 20) { num2str = textForms[2]; };
     if (n1 > 1 && n1 < 5) { num2str = textForms[1]; };
     if (n1 == 1) { num2str = textForms[0]; }
+	console.log(n);
+	console.log(n1);
+	console.log(num2str);
+	return `${yourBalance}${bonus}${num2str}`;
+	}
 
-    return `${yourBalance}${bonus} ${num2str}`;
-}
-
+formatGamePoints`Ваш баланс: ${arrayPrices}`;
 const output = formatGamePoints`Ваш баланс: ${arrayPrices}`;
 console.log(output);
 
